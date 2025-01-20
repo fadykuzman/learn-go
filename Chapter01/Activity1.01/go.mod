234517b746c5ddeada5cef01f4744520dc4a4eb3
Fady Kuzman <fady.kuzman@specificobject.de>
2025-01-20 11:54:17 +0100

    ex102 and activity101

---
+++ Chapter01/Activity1.01/go.mod
@@ -0,0 +1,3 @@
+module aact101
+
+go 1.23.3
---
+++ Chapter01/Activity1.01/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	first_name := "Bob"
+	family_name := "Smith"
+	age := 34
+	has_peanut_allergy := false
+	fmt.Println(first_name)
+	fmt.Println(family_name)
+	fmt.Println(age)
+	fmt.Println(has_peanut_allergy)
+}
