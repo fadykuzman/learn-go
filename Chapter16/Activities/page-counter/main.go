2d5ce59b91e98f12d5e6422a9753f1da1e835151
Fady Kuzman <fady.kuzman@pm.me>
2025-01-27 21:59:24 +0100

    act1601

--- Chapter16/Activities/page-counter/main.go
+++ Chapter16/Activities/page-counter/main.go
@@ -7,20 +7,36 @@ import (
 )
 
 type PageWithCounter struct {
-	counter int
-	content string
-	heading string
+	Counter int    `json:"counter"`
+	Content string `json:"content"`
+	Heading string `json:"heading"`
 }
 
-func (p PageWithCounter) ServeHttp(w http.ResponseWriter, r *http.Request) {
+func (p *PageWithCounter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	p.Counter++
 	msg := fmt.Sprintf(`
 		<h1>%s</h1>
 		<p>%s</p>
 		<div>views: %d</div>
-	`, p.heading, p.content, p.counter)
+	`, p.Heading, p.Content, p.Counter)
 	w.Write([]byte(msg))
 }
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", PageWithCounter{}))
+	hello := PageWithCounter{
+		Heading: "Hello, World!",
+		Content: "Hey, There!",
+	}
+	ch1 := PageWithCounter{
+		Heading: "Chapter 1",
+		Content: "Chapter 1: How to start",
+	}
+	ch2 := PageWithCounter{
+		Heading: "Chapter 2",
+		Content: "Chapter 2: More Advanced Greetings",
+	}
+	http.Handle("/", &hello)
+	http.Handle("/chapter1", &ch1)
+	http.Handle("/chapter2", &ch2)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
