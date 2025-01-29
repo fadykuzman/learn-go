7ab422841b6fcbf69ce7073e7a03e66c36ab0bf9
Fady Kuzman <fady.kuzman@pm.me>
2025-01-29 22:07:30 +0100

    ex1702

---
+++ Chapter17/Exercises/01/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func getDataAndReturnResponse() string {
+	r, err := http.Get("https://www.google.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer r.Body.Close()
+
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(data)
+}
+
+func main() {
+	data := getDataAndReturnResponse()
+	log.Println(data)
+}
